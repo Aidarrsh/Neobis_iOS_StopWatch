788626204f95eaf3e2f15e15e132e12fc557c8d4
aidar <sharipov_ai@auca.kg>
2023-04-16 16:59:24 +0600

    Finish project 2  

--- Neobis_iOS_StopWatch/ViewController.swift
+++ Neobis_iOS_StopWatch/ViewController.swift
@@ -17,16 +17,64 @@ extension UIImage {
     }
 }
 
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+
+         func numberOfComponents(in pickerView: UIPickerView) -> Int {
+             return 3
+         }
+
+         func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+             switch component {
+             case 0:
+                 return 25
+             case 1, 2:
+                 return 60
+             default:
+                 return 0
+             }
+         }
+
+         func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+             return pickerView.frame.size.width/3
+         }
+
+         func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+             switch component {
+             case 0:
+                 return "\(row) "
+             case 1:
+                 return "\(row) "
+             case 2:
+                 return "\(row) "
+             default:
+                 return ""
+             }
+         }
+         func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+             switch component {
+             case 0:
+                 hour = row
+             case 1:
+                 minutes = row
+             case 2:
+                 seconds = row
+             default:
+                 break;
+             }
+         }
+     }
+
 class ViewController: UIViewController {
     
-    @IBOutlet weak var pickerView: UIPickerView!
-    
+
+    @IBOutlet var pickerView: UIPickerView!
     @IBOutlet weak var TimeLabel: UILabel!
     @IBOutlet weak var Image: UIImageView!
     @IBOutlet weak var PlayButton: UIButton!
     @IBOutlet weak var StopButton: UIButton!
     @IBOutlet weak var PauseButton: UIButton!
     
+    var segmentControl = false
     var playButtonPressed = false
     var pauseButtonPressed = false
     var stopButtonPressed = false
@@ -34,6 +82,9 @@ class ViewController: UIViewController {
     var startTime:Date?
     var stopTime:Date?
     var scheduledTimer: Timer!
+    var hour: Int = 0
+    var minutes: Int = 0
+    var seconds: Int = 0
     
     let userDefaults = UserDefaults.standard
     let START_TIME_KEY = "starTime"
@@ -43,8 +94,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        pickerView.dataSource = self
+        pickerView.delegate = self
         pickerView.isHidden = true
+        
+        hour = pickerView.selectedRow(inComponent: 0)
+        minutes = pickerView.selectedRow(inComponent: 1)
+        seconds = pickerView.selectedRow(inComponent: 2)
         
         startTime = userDefaults.object(forKey: START_TIME_KEY) as? Date
         stopTime = userDefaults.object(forKey: STOP_TIME_KEY) as? Date
@@ -94,27 +150,73 @@ class ViewController: UIViewController {
     
     @IBAction func PlayButtonAction(_ sender: UIButton) {
         
-        let playImage = UIImage(systemName: "play.circle")?.withRenderingMode(.alwaysTemplate).resized(to: CGSize(width: 80, height: 80))
-        PlayButton.setImage(playImage?.withTintColor(.black, renderingMode: .alwaysOriginal), for: .normal)
-        
-        let pauseImage = UIImage(systemName: "pause.circle.fill")?.withRenderingMode(.alwaysTemplate).resized(to: CGSize(width: 80, height: 80))
-        PauseButton.setImage(pauseImage?.withTintColor(.black, renderingMode: .alwaysOriginal), for: .normal)
-        PauseButton.imageView?.contentMode = .scaleAspectFit
-        playButtonPressed = true
-        
-        if timerCounting {
+        if playButtonPressed == false {
+            let playImage = UIImage(systemName: "play.circle")?.withRenderingMode(.alwaysTemplate).resized(to: CGSize(width: 80, height: 80))
+            PlayButton.setImage(playImage?.withTintColor(.black, renderingMode: .alwaysOriginal), for: .normal)
             
-        } else {
-            if let stop = stopTime{
-                let restartTime = calcRestartTime(start: startTime!, stop: stop)
-                setStopTime(date: nil)
-                setStartTime(date: restartTime)
+            let pauseImage = UIImage(systemName: "pause.circle.fill")?.withRenderingMode(.alwaysTemplate).resized(to: CGSize(width: 80, height: 80))
+            PauseButton.setImage(pauseImage?.withTintColor(.black, renderingMode: .alwaysOriginal), for: .normal)
+            PauseButton.imageView?.contentMode = .scaleAspectFit
+            playButtonPressed = true
+            
+            if segmentControl == false{
+                if timerCounting {
+                    
+                } else {
+                    if let stop = stopTime{
+                        let restartTime = calcRestartTime(start: startTime!, stop: stop)
+                        setStopTime(date: nil)
+                        setStartTime(date: restartTime)
+                    } else {
+                        setStartTime(date: Date())
+                    }
+                    startTimer()
+                }
             } else {
+                scheduledTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+                timerCounting = true
                 setStartTime(date: Date())
             }
-            startTimer()
         }
     }
+    
+    func stopCountTimer() {
+        scheduledTimer.invalidate()
+        timerCounting = false
+        setStopTime(date: Date())
+    }
+
+    @objc func updateTimer() {
+        if hour == 0 && minutes == 0 && seconds == 0 {
+            stopCountTimer()
+            imagesSetToDef()
+            resetTimer()
+            return
+        }
+        
+        if seconds > 0 {
+            seconds -= 1
+        } else {
+            if minutes > 0 {
+                minutes -= 1
+                seconds = 59
+            } else {
+                hour -= 1
+                minutes = 59
+                seconds = 59
+            }
+        }
+        
+        setTimeLabel()
+    }
+    
+    func setTimeLabel() {
+        let hourString = String(format: "%02d", hour)
+        let minutesString = String(format: "%02d", minutes)
+        let secondsString = String(format: "%02d", seconds)
+        TimeLabel.text = "\(hourString):\(minutesString):\(secondsString)"
+    }
+
     
     func calcRestartTime(start: Date, stop: Date) -> Date{
         let diff = start.timeIntervalSince(stop)
@@ -131,6 +233,16 @@ class ViewController: UIViewController {
             scheduledTimer.invalidate()
         }
         setTimerCounting(val: false)
+    }
+    
+    func resetTimer(){
+        setStopTime(date: nil)
+        setStartTime(date: nil)
+        TimeLabel.text = makeTimeString(hour: 0, min: 0, sec: 0)
+        stopTimer()
+        pauseButtonPressed = false
+        playButtonPressed = false
+        
     }
     
     func setTimeLabel(_ val: Int){
@@ -183,34 +295,54 @@ class ViewController: UIViewController {
             setStopTime(date: Date())
             stopTimer()
         }
+        playButtonPressed = false
     }
     
-    @IBAction func StopButtonAction(_ sender: UIButton) {
-        
+    func imagesSetToDef(){
         let playImage = UIImage(systemName: "play.circle.fill")?.withRenderingMode(.alwaysTemplate).resized(to: CGSize(width: 80, height: 80))
         PlayButton.setImage(playImage?.withTintColor(.black, renderingMode: .alwaysOriginal), for: .normal)
         PlayButton.imageView?.contentMode = .scaleAspectFit
         
+        
         let pauseImage = UIImage(systemName: "pause.circle.fill")?.withRenderingMode(.alwaysTemplate).resized(to: CGSize(width: 80, height: 80))
         PauseButton.setImage(pauseImage?.withTintColor(.black, renderingMode: .alwaysOriginal), for: .normal)
         PauseButton.imageView?.contentMode = .scaleAspectFit
-        stopButtonPressed = true
-        pauseButtonPressed = false
-        playButtonPressed = false
+    }
+    
+    @IBAction func StopButtonAction(_ sender: UIButton) {
         
-        setStopTime(date: nil)
-        setStartTime(date: nil)
-        TimeLabel.text = makeTimeString(hour: 0, min: 0, sec: 0)
-        stopTimer()
+        imagesSetToDef()
+        stopButtonPressed = true
+        
+        if segmentControl == true {
+            stopCountTimer()
+            hour = pickerView.selectedRow(inComponent: 0)
+            minutes = pickerView.selectedRow(inComponent: 1)
+            seconds = pickerView.selectedRow(inComponent: 2)
+            setTimeLabel()
+            setStopTime(date: nil)
+        }
+        
+        resetTimer()
+        playButtonPressed = false
+        stopButtonPressed = false
     }
     
     @IBAction func didChangeSegment(_ sender: UISegmentedControl){
         if sender.selectedSegmentIndex == 0{
             Image.image = UIImage(systemName: "timer")
             pickerView.isHidden = true
+            segmentControl = false
+            resetTimer()
+            imagesSetToDef()
         } else {
             Image.image = UIImage(systemName: "stopwatch")
             pickerView.isHidden = false
+            segmentControl = true
+            resetTimer()
+            imagesSetToDef()
         }
     }
+    
 }
+
